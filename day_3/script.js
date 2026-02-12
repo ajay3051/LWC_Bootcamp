7dd40df2faaa751795eb78b4a844fec1763c83dc
ajay <ajay@gmail.com>
2026-02-12 20:21:02 +0530

    Spread operator code updated

--- day_3/script.js
+++ day_3/script.js
@@ -46,3 +46,81 @@ console.log(myCountry);
 
 let myDOB = myDetails.DOB;
 console.log(myDOB);
+
+// Spread Operator
+console.log("Spread Operator");
+
+// Usages of spread operator
+// Expanding string
+// Combining arrays
+// Combining oject
+// Creating new shallow copy of arrays and objects
+
+// concatination of arrays
+let numberOne = [1, 2, 3];
+let numberTwo = [4, 5, 6];
+
+console.log(numberOne);
+console.log(numberTwo);
+
+let combinedArray = [...numberOne, ...numberTwo];
+console.log(combinedArray);
+
+// expand the string
+let myName = "My name is ajay kumar";
+console.log(myName);
+console.log(...myName);
+
+// add elements in array
+numberOne = [...numberOne, 10, 15, 20];
+console.log(numberOne);
+
+numberOne = [-15, 0, ...numberOne, 22, 56];
+console.log(numberOne);
+
+// adding the object
+let myObj1 = {
+  fName: "Ajay",
+  Skills: ["Admin", "Apex", "LWC"],
+};
+console.log(myObj1);
+
+let myObj2 = {
+  lName: "Kumar",
+  City: "Pamarru",
+};
+console.log(myObj2);
+
+let finalObject = { ...myObj1, ...myObj2 };
+console.log(finalObject);
+
+// copy an array and object
+let myFavFruits = ["Apple", "Banana", "Mango"];
+console.log(myFavFruits);
+
+let myFavFruitsCopy = myFavFruits;
+console.log("Copy: ", myFavFruitsCopy);
+myFavFruitsCopy.push("Goa");
+console.log("Original", myFavFruits);
+console.log("Deep Copy", myFavFruitsCopy);
+
+let myFavCities = ["Vijayawada", "Guntur", "Hyderabad"];
+
+let myFavCitiesCopy = [...myFavCities];
+
+console.log("Original", myFavCities);
+console.log("Copy", myFavCitiesCopy);
+
+myFavCitiesCopy.push("Chennai");
+console.log("Original", myFavCities);
+console.log("Shallow Copy", myFavCitiesCopy);
+
+myFavCities.push("Kakinada");
+console.log(myFavCities);
+console.log(myFavCitiesCopy);
+
+let myDetailsCopy = { ...myDetails };
+myDetailsCopy["Pincode"] = "521157";
+
+console.log("Original", myDetails);
+console.log("Shallow Copy", myDetailsCopy);

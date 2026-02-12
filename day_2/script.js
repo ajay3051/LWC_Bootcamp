7a4d92f69d185bb391296f3c67990ed60a6e702e
ajay <ajay@gmail.com>
2026-02-12 11:09:04 +0530

    code modified with truthy and falsy values

--- day_2/script.js
+++ day_2/script.js
@@ -1,1 +1,20 @@
 console.log("LWC bootcamp day-2");
+
+let myName;
+console.log(typeof myName);
+
+if (myName) {
+  console.log("Print my name: " + myName);
+} else {
+  console.log("Name is not defined correctly!");
+}
+
+let mySalary = 0;
+console.log("Salary: " + mySalary);
+console.log(typeof mySalary);
+
+if (mySalary) {
+  console.log("You can buy a drink!");
+} else {
+  console.log("Your bank balance is zero!");
+}
